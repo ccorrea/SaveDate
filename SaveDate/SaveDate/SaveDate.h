4eae8d4809e9b44e204f7d8809d0c9d6be00e7ff
Christian Correa <christian@twelfthstation.com>
2017-10-02 21:30:41 -0700

    Added Xcode project.

---
+++ SaveDate/SaveDate/SaveDate.h
@@ -0,0 +1,19 @@
+//
+//  SaveDate.h
+//  SaveDate
+//
+//  Created by Christian Correa on 10/2/17.
+//  Copyright © 2017 Twelfth Station Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SaveDate.
+FOUNDATION_EXPORT double SaveDateVersionNumber;
+
+//! Project version string for SaveDate.
+FOUNDATION_EXPORT const unsigned char SaveDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaveDate/PublicHeader.h>
+
+
